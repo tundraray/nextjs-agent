1581586cd86604c1790c6989dbd1e48ac0de5c97
Mikhail Andreev <tundraray@outlook.com>
2025-05-08 01:21:56 +0300

    refactor: remove default retrieval text and update document state initialization
    - Deleted the DefaultRetrievalText file to simplify the codebase and remove unnecessary dependencies.
    - Updated the UploadDocumentsForm component to initialize the document state as an empty string instead of using the removed default text.

--- app/components/UploadDocumentsForm.tsx
+++ app/components/UploadDocumentsForm.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import { useState, type FormEvent } from "react";
-import DEFAULT_RETRIEVAL_TEXT from "@/data/DefaultRetrievalText";
 import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
 
 export function UploadDocumentsForm() {
   const [isLoading, setIsLoading] = useState(false);
-  const [document, setDocument] = useState(DEFAULT_RETRIEVAL_TEXT);
+  const [document, setDocument] = useState('');
   const ingest = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsLoading(true);
